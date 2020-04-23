4606c250941a98d255228795f0ffe0585c408ac8
Michael Hernandez <gammabeta010@gmail.com>
2020-04-23 11:51:18 +0200

    WIP Step 2

---
+++ data.php
@@ -0,0 +1,31 @@
+<?php
+
+    $graphs = [
+        'fatturato' =>
+        [
+            'type' => 'line',
+            'data' => [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322]
+        ],
+
+        'fatturato_by_agent' =>
+        [
+            'type' => 'pie',
+            'data' =>
+            [
+                'Marco' => 9000,
+                'Giuseppe' => 4000,
+                'Mattia' => 3200,
+                'Alberto' => 2300
+            ]
+        ]
+    ];
+
+    foreach ($graphs as $key => $graph) {
+        echo 'This is the type of graph: ' .$graph[type] . '<br><br>';
+         var_dump($graph[data]);
+         echo '<br><br>';
+
+    }
+
+
+?>
--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-    include 'data_step1.php';
+    include 'data.php';
     header('Content-Type: application/json');
     echo json_encode($data);
 ?>
